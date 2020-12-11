1119c06fecaaf385c7e2831145b39abf8e96367a
CthulhuPH <ctulhu@tuta.io>
2020-12-11 16:07:50 +0800

    First Template Version (Unfinished)

---
+++ js/pop.js
@@ -0,0 +1,14 @@
+function ADDCOPY() {
+  /* Get the text field */
+  var copyText = document.getElementById("BTCADDRESS");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
